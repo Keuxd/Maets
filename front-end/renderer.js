a04a09ad76160bb3d9bc1315f7b3bd754fbfce49
Alessandra <95037488+Cassie91@users.noreply.github.com>
2023-06-07 21:15:00 -0300

    Button settings
    Button configured, however, for some reason, the page is refreshing when pressed.

--- front-end/renderer.js
+++ front-end/renderer.js
@@ -5,7 +5,7 @@ ipcRenderer.on('java-backend-response', (event, response) => {
     messageElement.textContent = response;
 });
 
-ipcRenderer.on('log-message', (event, message) => {
+/* ipcRenderer.on('log-message', (event, message) => {
     const logElement = document.getElementById('log');
     logElement.textContent = message;
 })
@@ -14,4 +14,15 @@ const myButton = document.getElementById('button');
 
 myButton.addEventListener('click', () => {
     ipcRenderer.send('send-to-backend', 'buttonPress');
-})
+})*/
+
+const logincard = document.getElementById('logincard');
+const anotherScreen = document.getElementById('anotherScreen');
+const loginButton = document.getElementById('loginButton');
+const loginForm = document.querySelector('form');
+
+loginButton.addEventListener('click', () => {
+    logincard.remove;
+    anotherScreen.style.display = 'block';
+    anotherScreen.style.visibility = 'visible';
+  });
