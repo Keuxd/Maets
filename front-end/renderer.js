cc18a4c8a99f87335da5b27f5f8aeaadf7aa6387
Keu <keuxd2003@gmail.com>
2023-06-12 00:25:11 -0300

    Login screen now is invoked via function

--- front-end/renderer.js
+++ front-end/renderer.js
@@ -1,55 +1,98 @@
 const { ipcRenderer } = require('electron');
 const { LoginResponseHandler, isValidEmail } = require('./auth');
-
-let loginResponseCode;
-
-ipcRenderer.on('java-backend-response', (event, response) => {
-    console.log('Response:', response);
-    
-    if (response && response.startsWith('login ')) {
-      const responseCode = response.split(' ')[1];
-      loginResponseCode = responseCode.replace('\r\n', '').replace(' ','');
   
-      const numericCode = responseCode;
-      LoginResponseHandler.handle({ code: numericCode });
-      console.log('response: ', loginResponseCode);
-      if (loginResponseCode === '0') {
-        console.log(`${emailInput.value} ${passwordInput.value}`);
-        loadscreen.style.display = 'none';
-        anotherScreen.style.display = 'block';
-      } else {
-        loginform.reportValidity();
-        loadscreen.style.display = 'none';
-        logincard.style.display = 'block';
-      }
+ipcRenderer.on('java-backend-response', (event, response) => {
+    switch(response[0]) {
+        case 'login' :
+            switch(response[1]){
+                case "0" :
+                    document.getElementById("loadingScreen").style.display = "none";
+                    document.getElementById("anotherScreen").style.display = "block";
+                    document.getElementById("logincard").remove();
+                    break;
+                default :
+                    document.getElementById("error").textContent = LoginResponseHandler.responseMessages[response[1]].message;
+                    document.querySelector('form').reportValidity();
+                    document.getElementById("loadingScreen").style.display = "none";
+                    document.getElementById("logincard").style.display = "block";
+                    break;
+            }
+            break;
+
+        case 'isLoggedIn' : 
+            if(response[1] === 'true') showAnotherScreen();
+            else invokeLoginCard();
+            break;
     }
-    console.log('logincode:', loginResponseCode);
 });
 
-const loginform = document.querySelector('form');
-const logincard = document.getElementById('logincard');
-const loadscreen = document.getElementById('loadingScreen');
-const anotherScreen = document.getElementById('anotherScreen');
-const loginButton = document.getElementById('loginButton');
-const logoutButton = document.getElementById('logoutButton');
-const emailInput = document.getElementById('email');
-const passwordInput = document.getElementById('password');
+function invokeLoginCard() {
+    const logincard = document.createElement("div");
+        logincard.id = "logincard";
+    const title = document.createElement("h1");
+        title.id = "title";
+        title.textContent = "Welcome!";
+    const subtitle = document.createElement("h2");
+        subtitle.textContent = "Login or signup below.";
+        subtitle.id = "subtitle";
+    const form = document.createElement("form");
+        const usernameLabel = document.createElement("label");
+            usernameLabel.htmlFor = "username";
+            usernameLabel.className = "attfield";
+            usernameLabel.textContent = "EMAIL:";
+        const inputEmail = document.createElement("input");
+            inputEmail.type = "text";
+            inputEmail.id = "email";
+            inputEmail.className = "attinput";
+            inputEmail.required = true;
+        const passwordLabel = document.createElement("label");
+            passwordLabel.htmlFor = "password";
+            passwordLabel.className = "attfield";
+            passwordLabel.textContent = "PASSWORD:";
+        const inputPassword = document.createElement("input");
+            inputPassword.type = "password";
+            inputPassword.id = "password";
+            inputPassword.className = "attinput";
+            inputPassword.required = true;
+        const breakLine = document.createElement("br");
+        const loginButton = document.createElement("button");
+            loginButton.id = "loginButton";
+            loginButton.type = "button";
+            loginButton.className = "attscreen";
+            loginButton.textContent = "LOGIN";
+    const divError = document.createElement("div");
+        divError.id = "error";
 
-logoutButton.addEventListener('click', (event) => {
-  ipcRenderer.send('send-to-backend', "logout");
-  anotherScreen.style.display = 'none';
-  logincard.style.display = 'block';
-})
-
-loginButton.addEventListener('click', (event) => {
-    event.preventDefault();
+    loginButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        
+        if (form.checkValidity() && isValidEmail(inputEmail.value)) {
+          logincard.style.display = 'none';
+          document.getElementById('loadingScreen').style.display = "block";
     
-    if (loginform.checkValidity() && isValidEmail(emailInput.value)) {
-      logincard.style.display = 'none';
-      loadscreen.style.display = 'block';
-
-      ipcRenderer.send('send-to-backend', `login ${emailInput.value} ${passwordInput.value}`);
-      } else {
-        loginform.reportValidity();
-      }
+          ipcRenderer.send('send-to-backend', `login ${inputEmail.value} ${inputPassword.value}`);
+        } else {
+            form.reportValidity();
+        }
     });
+
+    document.getElementById("loadingScreen").hidden = true;
+    form.append(usernameLabel, inputEmail, passwordLabel, inputPassword, breakLine, loginButton);
+    logincard.append(title, subtitle, form, divError);
+    document.body.appendChild(logincard);
+}
+
+function showAnotherScreen() {
+    const logoutButton = document.getElementById("logoutButton");
+    const anotherScreen = document.getElementById("anotherScreen");
+    logoutButton.addEventListener('click', (event) => {
+        console.log("LOGOUT CLICK");
+        ipcRenderer.send('send-to-backend', 'logout');
+        invokeLoginCard();
+        anotherScreen.style.display = "none";
+    });
+
+    anotherScreen.hidden = false;
+    anotherScreen.style.display = "block";
+    document.getElementById("loadingScreen").style.display = "none";
+}
