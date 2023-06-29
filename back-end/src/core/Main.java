1af12702fb620d088b7ee18c3e24d2fb751a9ab8
Keu <keuxd2003@gmail.com>
2023-06-29 18:52:11 -0300

    Configuration files are now being intialized

--- back-end/src/core/Main.java
+++ back-end/src/core/Main.java
@@ -1,9 +1,18 @@
 package core;
 
+import java.io.IOException;
+
+import config.LocalConfigs;
+import config.OnlineConfigs;
+
 public class Main {
 	
-	public static void main(String[] args) throws InterruptedException {
+	
+	public static void main(String[] args) throws IOException {
 		Electron.initTCPConnection();
+		
+		OnlineConfigs.initialize(MAETS_FOLDER_PATH);
+		LocalConfigs.initialize(MAETS_FOLDER_PATH);
 		
 		System.out.println("Finish main thread");
 	}
