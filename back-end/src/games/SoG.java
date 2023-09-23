28b20b2f5a6c4a666a26802d3d9dcad522515fdd
Keu <keuxd2003@gmail.com>
2023-09-23 01:54:55 -0300

    Added LocalGameFolderPath in constructor

--- back-end/src/games/SoG.java
+++ back-end/src/games/SoG.java
@@ -1,13 +1,22 @@
 package games;
 
+import java.io.File;
+
+import config.LocalConfigs;
+import core.FileSplitMerge;
+import core.Main;
+import core.ZipExtractor;
+import download.cloudservices.GitHub;
+
 public class SoG extends AbstractGame {
 
 	public SoG() {
 		super(
 				0,
 				"Secrets Of Grindea",
-				"Secrets of Grindea is an old-school Action RPG with co-op support for up to 4 players.\nIt is a tribute to and sometimes a parody of the old SNES games so many of us grew up with and loved!\nJourney through fantastical lands and battle tons of different enemies and bosses in your quest for truth, friendship and, above all, finding the world's rarest treasures!"
-			 );
+				"Secrets of Grindea is an old-school Action RPG with co-op support for up to 4 players.\nIt is a tribute to and sometimes a parody of the old SNES games so many of us grew up with and loved!\nJourney through fantastical lands and battle tons of different enemies and bosses in your quest for truth, friendship and, above all, finding the world's rarest treasures!",
+				Main.GAMES_FOLDER_PATH + "Secrets Of Grindea/"
+				);
 		
 	}
 	
