9d3145c0b77e034a5b3c471b38f11196ebd16703
Keu <keuxd2003@gmail.com>
2023-09-23 01:54:31 -0300

    Added LocalGameFolderPath in constructor
    yep, yep, do that

--- back-end/src/games/VS.java
+++ back-end/src/games/VS.java
@@ -1,13 +1,17 @@
 package games;
 
+import core.Main;
+
 public class VS extends AbstractGame {
 	
 	public VS() {
 		super(
 				1,
 				"Vampire Survivors",
-				"Vampire Survivors is a casual gothic horror game with roguelite elements where your choices can make you grow quickly and annihilate the thousands of monsters that appear along the way.\nVampire Survivors is a survival game with minimalist gameplay and roguelite elements.\nHell is empty. All the demons are here, and there is no way to hide. All he can do is survive as long as he can until death finally ends his suffering. Collect gold in matches to buy upgrades and help the next survivor.\nThe supernatural indie phenomenon that turns you into a hail of bullets!"
-			 );
+				"Vampire Survivors is a casual gothic horror game with roguelite elements where your choices can make you grow quickly and annihilate the thousands of monsters that appear along the way.\nVampire Survivors is a survival game with minimalist gameplay and roguelite elements.\nHell is empty. All the demons are here, and there is no way to hide. All he can do is survive as long as he can until death finally ends his suffering. Collect gold in matches to buy upgrades and help the next survivor.\nThe supernatural indie phenomenon that turns you into a hail of bullets!",
+				Main.GAMES_FOLDER_PATH + "Vampire Survivors/"
+				);
+	}
 
 	@Override
 	public void download() throws Exception {}
