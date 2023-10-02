f26bfd8d9a6e118d0a2d671963acdd47d929a5a2
Keu <keuxd2003@gmail.com>
2023-10-02 16:40:19 -0300

    New game added

---
+++ back-end/src/games/DSCSCE.java
@@ -0,0 +1,46 @@
+package games;
+
+import core.Main;
+
+public class DSCSCE extends AbstractGame {
+
+	public DSCSCE() {
+		super(
+				2,
+				"Digimon Story Cyber Sleuth Complete Edition",
+				"With engaging storylines, classic turn-based battles, and tons of Digimon to collect, Digimon Story Cyber Sleuth: Complete Edition delivers everything fans loved about Digimon Story: Cyber Sleuth and Digimon Story: Cyber Sleuth – Hacker’s Memory.\nGet the full experience with the Complete Edition which includes both titles in one!\n",
+				Main.GAMES_FOLDER_PATH + "Digimon Story Cyber Sleuth Complete Edition/"
+				);
+	}
+
+	@Override
+	public void download(int serverId) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isDownloadAvailable(int serverId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void install() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void uninstall() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
