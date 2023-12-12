8a15e56b4102bd5783e43bff7dffba55cb9412b7
Keu <keuxd2003@gmail.com>
2023-12-12 15:43:08 -0300

    fatalErrorPopup() changed sides
    I don't remember why but it did

--- front-end/scripts/node_environment/UtilNode.js
+++ front-end/scripts/node_environment/UtilNode.js
@@ -3,6 +3,19 @@ const { WindowsNode } = require("./WindowsNode");
 
 class UtilNode {
 	
+	static fatalErrorPopup(title, content) {
+		if(WindowsNode.MAIN_WINDOW != null)	
+			WindowsNode.MAIN_WINDOW.hide();
+		
+		dialog.showErrorBox(title, content);
+		app.quit();
+	}
+	
+	static sendToWebContext(message) {
+		WindowsNode.MAIN_WINDOW.webContents.send("node-message", message);
+	}
+	
+	// Legacy
 	static isJson(string) {
 		try {
 			return [true, JSON.parse(string)];
@@ -10,13 +23,6 @@ class UtilNode {
 			return [false, null];
 		}
 	}
-	
-	static fatalErrorPopup(title, content) {
-		WindowsNode.MAIN_WINDOW.hide();
-		dialog.showErrorBox(title, content);
-		app.quit();
-	}
-	
 	
 }
 
